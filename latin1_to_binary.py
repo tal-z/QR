577e05e1ff3a5caa8e8f7b9c1308017e1916dd69
Tal Zaken <talzaken@gmail.com>
2022-01-16 14:59:41 -0500

    Added functions to take a latin1 string and turn it into a binary string that meets the needs of a Version 1, Error Correction Level M QR code.

--- latin1_to_binary.py
+++ latin1_to_binary.py
@@ -1,4 +1,47 @@
 
-ba = bytearray('http://talzaken.com', 'latin-1')
-binary = [bin(b) for b in ba]
-print(binary)
+def pad_binary(string):
+    pad = 8 - (len(string) - 2)
+    return '0' * pad + string[2:]
+
+
+def latin1_to_bitstring(string):
+    ba = bytearray(string, 'latin-1')
+    binary = [pad_binary(bin(b)) for b in ba]
+    return ''.join(binary)
+
+
+def terminate_bitstring(bitstring, required_bits=128):
+    """adds a terminator to the end of a bitstring, based on a required padding length"""
+    bs_len = len(bitstring)
+    if bs_len > required_bits:
+        raise ValueError("Bitstring is too long for the number of bits required by this version and error correction level.")
+    if bs_len == required_bits:
+        return bitstring
+    elif required_bits - bs_len < 4:
+        return bitstring + '0' * (required_bits - bs_len)
+    else:
+        return bitstring + '0000'
+
+
+def make_8_divisible_bitstring(bitstring):
+    bs_len = len(bitstring)
+    mod = bs_len % 8
+    if not mod:
+        return bitstring
+    else:
+        return bitstring + '0' * mod
+
+
+
+if __name__ == '__main__':
+    s = 'talzaken.com'
+    bs = latin1_to_bitstring(s)
+    print(bs)
+    print(len(bs))
+
+    tbs = terminate_bitstring(bs)
+    print(len(tbs))
+    ptbs = make_8_divisible_bitstring(tbs)
+    print(len(ptbs))
+
+
