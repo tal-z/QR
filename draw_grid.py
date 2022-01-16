24f1b59186e959d80d8c3cc77f57563d4fd6b4d5
Tal Zaken <talzaken@gmail.com>
2022-01-16 11:17:29 -0500

    added functions to draw finder patterns and timing patterns on a Version 1 QR Code, based on a numpy matrix of zeros.

--- draw_grid.py
+++ draw_grid.py
@@ -34,7 +34,57 @@ cmap = colors.ListedColormap(['white', 'black'])
 
 plt.imshow(version1_base_data, cmap=cmap)
 
-# draw gridlines
-
 plt.show()
 '''
+
+v1_base_grid = np.zeros((21, 21))
+
+def draw_finder_patterns(grid):
+    """fills in corners of grid with easily-recognized square QR pattern"""
+    for i, row in enumerate(grid):
+        for j, square in enumerate(row):
+            # Draw horizontal outer lines
+            if (i == 0 or i == 6 or i == 14 or i == 20) and (j < 7):
+                grid[i, j] = 1
+            if (i == 0 or i == 6) and (j > 14):
+                grid[i, j] = 1
+            # Draw vertical outer lines
+            if (j == 0 or j == 6 or j == 14 or j == 20) and (i < 7):
+                grid[i, j] = 1
+            if (j == 0 or j == 6) and (i > 14):
+                grid[i, j] = 1
+            # Draw inner squares
+            if (1 < i < 5) and (1 < j < 5):
+                grid[i, j] = 1
+            if (15 < i < 19) and (1 < j < 5):
+                grid[i, j] = 1
+            if (15 < j < 19) and (1 < i < 5):
+                grid[i, j] = 1
+
+
+def draw_timing_patterns(grid):
+    """
+    Fills in relevant area with evenly-spaced pattern of alternating black and white modules,
+    used to tell the scanner the size of modules and the version of QR code being interpreted.
+    This is the Version 1 timing pattern.
+    """
+    for i, row in enumerate(grid):
+        for j, square in enumerate(row):
+            if (i == 6) and (7 < j < 13) and (j % 2 == 0):
+                grid[i, j] = 1
+            if (j == 6) and (7 < i < 13) and (i % 2 == 0):
+                grid[i, j] = 1
+
+
+
+print(v1_base_grid)
+draw_finder_patterns(v1_base_grid)
+draw_timing_patterns(v1_base_grid)
+print(v1_base_grid)
+
+
+
+# Plot and show the grid
+cmap = colors.ListedColormap(['white', 'black'])
+plt.imshow(v1_base_grid, cmap=cmap)
+plt.show()
