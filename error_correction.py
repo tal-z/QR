854c7341e804bd03f37b1bdebd0b7a55f7a7f026
Tal Zaken <talzaken@gmail.com>
2022-01-17 12:30:01 -0500

    now using reedsolo package to generate codewords. functinalized codeword generation.

--- error_correction.py
+++ error_correction.py
@@ -1,4 +1,4 @@
-## Early Attempts
+'''## Early Attempts
 """
 const LOG = new Uint8Array(256);
 const EXP = new Uint8Array(256);
@@ -36,4 +36,50 @@ def div(a, b):
     return exp[((log[a] + log[b] * 254) % 255)]
 
 print(mul(16, 32))
+
+'''
+from reedsolo import RSCodec
+from helpers import pad_binary, VerboseControl
+
+
+
+def generate_code_words(string, verbose=True):
+    with VerboseControl(verbose=verbose):
+        print("Encoding string as ascii.")
+        b_string = string.encode('ascii')
+        print("~~ascii string:", b_string)
+        print("Initializing Reed-Solomon Codec for calculating error correction codes.")
+        rsc = RSCodec(10) # 10 is the amount of space left in the QR code based on the Version and chosen Error Correction Level.
+        print('Encoding the binary string.')
+        code = rsc.encode(b_string)
+        print('~~encoded data with error correction:', *code)
+        print('Converting code words to binary.')
+        code_words = code[-10:]
+        binary_code_words = [pad_binary(bin(num)) for num in code_words]
+        print('~~binary code words:', binary_code_words)
+        print('Returning code words as binary.')
+        return binary_code_words
+
+if __name__ == '__main__':
+    generate_code_words('talzaken.com')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
