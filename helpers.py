51bfa838252f747cf64d0c69228740c2e777c59f
Tal Zaken <talzaken@gmail.com>
2022-01-17 12:29:11 -0500

    add context manager to helpers.py

--- helpers.py
+++ helpers.py
@@ -1,3 +1,20 @@
+import os
+import sys
+
+
+class VerboseControl:
+    def __init__(self, verbose=True):
+        self.verbose = verbose
+
+    def __enter__(self):
+        self._original_stdout = sys.stdout
+        if not self.verbose:
+            sys.stdout = open(os.devnull, 'w')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        #sys.stdout.close()
+        sys.stdout = self._original_stdout
+
 
 def pad_binary(string):
     pad = 8 - (len(string) - 2)
